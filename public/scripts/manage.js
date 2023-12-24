b2ad91728ce04d974f259c009414d0a3379cc315
SoJS-Coder <matfuller703@gmail.com>
2023-12-24 23:39:37 +0000

    fixed small things

--- public/scripts/manage.js
+++ public/scripts/manage.js
@@ -49,7 +49,7 @@ if(createRound){
 function handleCreateRound(data) {
     console.log(data);
     if (data.error) return displayNotif(data.message, "red");
-    var uid = data.uid
+    var uid = data.data.uid
     displayNotif("Round " + uid + " created", "green")
 }
 function handleCreateTourney(data) {
